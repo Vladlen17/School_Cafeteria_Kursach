51f3bf9636561d5540078c6ec9d8e149fa61b44e
Vladlen <vladlencherkashin2004@gmail.com>
2024-04-07 23:17:12 +0700

    Удалены лишние юзинги

--- Cafeteria.App/View/FoodByClassForm.cs
+++ Cafeteria.App/View/FoodByClassForm.cs
@@ -1,12 +1,4 @@
-﻿using Cafeteria.App.school_cafeteriaDataSetTableAdapters;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace Cafeteria.App
